f4f9208d683abdefa13d1af6d634aa9ab8a821d5
H-JEMAI <57859131+H-JEMAI@users.noreply.github.com>
2021-07-04 18:25:24 +0200

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hateeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeem
+          Learn React Ahmeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeed
         </a>
       </header>
     </div>
